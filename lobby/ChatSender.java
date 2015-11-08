0445dae7d5e0b33be9de68e8975e88b595959c89
ArnasB <xxl606@gmail.com>
2015-11-07 22:40:13 -0500

    broke something

--- lobby/ChatSender.java
+++ lobby/ChatSender.java
@@ -2,25 +2,27 @@ package lobby;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import serverCommunication.ServerInterface;
 import userInterface.LobbyWindow;
 
 public class ChatSender implements ActionListener {
 	
-	ServerInterface serverInterface;
-	LobbyWindow lobbyWindow;
+	private ServerInterface serverInterface;
+	private LobbyWindow lobbyWindow;
 	//String messageToSend;
 	
 	public ChatSender(ServerInterface serverInterface, LobbyWindow lobbyWindow) {
 		this.serverInterface = serverInterface;
 		this.lobbyWindow = lobbyWindow;
 	}
+	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		String messageToSend = lobbyWindow.retrieveInputText();
-		serverInterface.sendMsg_All(messageToSend);
-		
+		serverInterface.sendMsg_All(messageToSend);		
 	}
 
 }
--- userInterface/LobbyWindow.java
+++ userInterface/LobbyWindow.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import java.awt.event.ActionListener;
 import java.awt.event.AdjustmentEvent;
 import java.awt.event.AdjustmentListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
 import com.jgoodies.forms.layout.FormLayout;
@@ -18,6 +20,7 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JTextArea;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingUtilities;
 import javax.swing.border.Border;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
@@ -25,6 +28,7 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import javax.swing.JPanel;
 
 public class LobbyWindow implements LobbyWindowInterface{
 
@@ -33,8 +37,11 @@ public class LobbyWindow implements LobbyWindowInterface{
 	private JTextArea chatTxtArea;
 	private JList<String> usrList;
 	private DefaultListModel<String> listModel;
-	private JButton disconnectButton;
+	private JButton btnDisconnect;
 	private JButton sendButton;
+	private JButton btnCreateTable;
+	private JPanel panel;
+	private JTextArea tableTxtArea;
 	/**
 	 * Create the application.
 	 */
@@ -73,53 +80,88 @@ public class LobbyWindow implements LobbyWindowInterface{
 		chatTxtField.setColumns(10);
 		chatTxtField.setBorder(border);
 		
-		disconnectButton = new JButton("Disconnect");
-		disconnectButton.setFont(new Font("Tahoma", Font.BOLD, 40));
+		btnDisconnect = new JButton("Disconnect");
+		btnDisconnect.setFont(new Font("Tahoma", Font.BOLD, 12));
 		
 		sendButton = new JButton("Send");
 		sendButton.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		
+		btnCreateTable = new JButton("Create Table");
+		btnCreateTable.setFont(new Font("Tahoma", Font.BOLD, 12));
+		
+		panel = new JPanel();
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
+			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 714, Short.MAX_VALUE)
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
 								.addComponent(chatScrollPane)
-								.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+								.addGroup(groupLayout.createSequentialGroup()
 									.addComponent(chatTxtField, GroupLayout.DEFAULT_SIZE, 472, Short.MAX_VALUE)
 									.addPreferredGap(ComponentPlacement.RELATED)
 									.addComponent(sendButton, GroupLayout.PREFERRED_SIZE, 59, GroupLayout.PREFERRED_SIZE)))
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(userScrollPane, GroupLayout.DEFAULT_SIZE, 171, Short.MAX_VALUE)
-							.addContainerGap())
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(disconnectButton, GroupLayout.PREFERRED_SIZE, 358, GroupLayout.PREFERRED_SIZE)
-							.addGap(72))))
+							.addComponent(userScrollPane, GroupLayout.DEFAULT_SIZE, 171, Short.MAX_VALUE))
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addComponent(btnCreateTable, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(btnDisconnect, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(73)
-					.addComponent(disconnectButton, GroupLayout.PREFERRED_SIZE, 279, GroupLayout.PREFERRED_SIZE)
-					.addGap(61)
+					.addContainerGap()
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 321, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnDisconnect)
+						.addComponent(btnCreateTable))
+					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(chatScrollPane, GroupLayout.DEFAULT_SIZE, 98, Short.MAX_VALUE)
+							.addComponent(chatScrollPane, GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
 							.addPreferredGap(ComponentPlacement.UNRELATED)
 							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 								.addComponent(chatTxtField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 								.addComponent(sendButton, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)))
-						.addComponent(userScrollPane, GroupLayout.DEFAULT_SIZE, 132, Short.MAX_VALUE))
+						.addComponent(userScrollPane, GroupLayout.DEFAULT_SIZE, 161, Short.MAX_VALUE))
 					.addGap(21))
 		);
+		panel.setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),}));
+		
+		tableTxtArea = new JTextArea();
+		tableTxtArea.setText("Tables");
+		tableTxtArea.setEditable(false);
+		panel.add(tableTxtArea, "2, 2, fill, fill");
 		frame.getContentPane().setLayout(groupLayout);
 		chatScrollPane.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener() {  
 	        public void adjustmentValueChanged(AdjustmentEvent e) {  
 	            e.getAdjustable().setValue(e.getAdjustable().getMaximum());  
 	        }
 	    });
+//		usrList.addMouseListener( new MouseAdapter()
+//		{
+//		    public void mousePressed(MouseEvent e)
+//		    {
+//		        if ( SwingUtilities.isRightMouseButton(e) )
+//		        {
+//		            JList list = (JList)e.getSource();
+//		            int row = list.locationToIndex(e.getPoint());
+//		            list.setSelectedIndex(row);
+//		        }
+//		    }
+//
+//		});
 	}
 	
 	public void setChatListener(ActionListener listener) {
@@ -128,8 +170,9 @@ public class LobbyWindow implements LobbyWindowInterface{
 	}
 	
 	public void setDisconnectListener(ActionListener listener) {
-		disconnectButton.addActionListener(listener);
+		btnDisconnect.addActionListener(listener);
 	}
+	
 	public void insertText(String text) {
 		chatTxtArea.append(text + "\n");
 	}
