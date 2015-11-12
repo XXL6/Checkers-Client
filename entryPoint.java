06f4352a97190f1c39773d2975a509567cd06c07
Jameswalker1222 <walker.227@wright.edu>
2015-11-12 13:53:26 -0500

    hello

--- entryPoint.java
+++ entryPoint.java
@@ -5,11 +5,11 @@ import serverCommunication.ServerCommunicator;
 import setup.LoginInitializer;
 
 public class entryPoint {
-	
+
 	public static void main(String[] args) {
 		Thread client = new MainClient();
 		client.start();
-		
+		//hello
 	}//end main
 
 }//end class
